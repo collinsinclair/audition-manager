09a9d42b14bb05e8c921d7c26a13055884bbe28b
Collin Sinclair <collin@collinsinclair.com>
2023-05-14 23:29:29 -0600

    add types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface auditionerProps {
+  status: string;
+  id: number;
+  fullName: string;
+  pronouns: string;
+  age: number;
+  studentCell: string;
+  studentEmail: string;
+  parentName: string;
+  parentCell: string;
+  parentEmail: string;
+}
